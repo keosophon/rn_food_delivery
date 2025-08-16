bbf9d9156969fd179b34647925c0d38c76d02a78
keosophon@gmail.com <keosophon8888@gmail.com>
2025-08-17 09:16:31 +1200

    fonts loaded

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,20 @@
-import { Stack } from "expo-router";
+import { useFonts } from "expo-font";
+import { SplashScreen, Stack } from "expo-router";
+import { useEffect } from "react";
 import "./global.css";
 
 export default function RootLayout() {
+  const [fontsLoaded] = useFonts({
+  "Quicksand-Regular": require('../assets/fonts/Quicksand-Regular.ttf'),
+  "Quicksand-Bold": require('../assets/fonts/Quicksand-Bold.ttf'),
+  "Quicksand-SemiBold": require('../assets/fonts/Quicksand-SemiBold.ttf'),
+  "Quicksand-Medium": require('../assets/fonts/Quicksand-Medium.ttf'),
+  "Quicksand-Light": require('../assets/fonts/Quicksand-Light.ttf'),
+});
+
+  useEffect(() => {
+    if (fontsLoaded) SplashScreen.hideAsync();
+    if (Error) throw Error;
+  }, [fontsLoaded, Error]);
   return <Stack />;
 }
