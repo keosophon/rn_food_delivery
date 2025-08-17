f9d42286220fa6d84160e52567f5ee1252ca24b0
keosophon@gmail.com <keosophon8888@gmail.com>
2025-08-17 14:43:13 +1200

    main page

--- app/index.tsx
+++ app/index.tsx
@@ -1,32 +1,46 @@
-import { offers } from '@/constants'
-import { Fragment } from 'react'
-import { FlatList, Pressable, View } from 'react-native'
-import { SafeAreaView } from 'react-native-safe-area-context'
+import { offers } from '@/constants';
+import cn from 'clsx';
+import { Fragment } from 'react';
+import { FlatList, Image, Pressable, View } from 'react-native';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 
 export default function App () {
   console.log(offers)
   return (
-    <SafeAreaView>
-      <FlatList
-        data={offers}
-        keyExtractor={(item) => item.id.toString()}
-        renderItem={({ item, index }) => {
-          return (
-            <View>
-              <Pressable className='bg-amber-600'>
-                {({pressed}) => (
-                  <Fragment>
-                    <View className={"h-full w-1/2"}>
-                      
-                    </View>
-                    <View></View>
-                  </Fragment>
-                )}
-              </Pressable>
-            </View>
-          )
-        }}
-      />
-    </SafeAreaView>
+    <SafeAreaProvider>
+      <SafeAreaView>
+        <FlatList
+          data={offers}
+          keyExtractor={item => item.id.toString()}
+          renderItem={({ item, index }) => {
+            const isEven: boolean = index % 2 === 0
+            return (
+              <View>
+                <Pressable
+                  className={cn(
+                    'offer-card',
+                    isEven ? 'flex-row-reverse' : 'flex-row'
+                  )}
+                  style={{ backgroundColor: item.color }}
+                >
+                  {({ pressed }) => (
+                    <Fragment>
+                      <View className='h-full w-1/2'>
+                        <Image
+                          source={item.image}
+                          className='size-full'
+                          resizeMode='contain'
+                        />
+                      </View>
+                      <View className='offer-card__info'>{item.title}</View>
+                    </Fragment>
+                  )}
+                </Pressable>
+              </View>
+            )
+          }}
+        />
+      </SafeAreaView>
+    </SafeAreaProvider>
   )
 }
