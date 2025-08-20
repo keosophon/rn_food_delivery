b999dd5c174c3255ee6245ab491020aacb8b99bb
keosophon@gmail.com <keosophon8888@gmail.com>
2025-08-21 10:30:06 +1200

    add ImageBackground and logo to _layout.tsx in (auth) folder

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -1,13 +1,23 @@
+
 import { Slot } from 'expo-router'
 import React from 'react'
-import { Text } from 'react-native'
+import { Dimensions, Image, ImageBackground, KeyboardAvoidingView, Platform, ScrollView, View } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
+import { images } from '../../constants'
 
 const _layout = () => {
   return (
     <SafeAreaView>
-      <Text>_layout</Text>
-      <Slot />
+      <KeyboardAvoidingView behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
+        <ScrollView className="bg-white h-full" keyboardShouldPersistTaps="handled">
+          <View className="w-full relative" style={{height:Dimensions.get('screen').height/2.25}}>
+            <ImageBackground source={images.loginGraphic} className="w-full h-full rounded-b-lg" resizeMode="stretch" />
+            <Image source={images.logo} className="absolute size-48 self-center -bottom-10  z-10" />
+          </View>
+          <Slot />
+        </ScrollView>
+        
+      </KeyboardAvoidingView>      
     </SafeAreaView>
   )
 }
--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,10 +1,10 @@
-import { Redirect, Slot } from 'expo-router'
-import React from 'react'
+import { Redirect, Slot } from "expo-router";
+import React from "react";
 
 const _layout = () => {
-    const isAuthenticated = false
-    if (!isAuthenticated) return <Redirect href="/sign_in" />
-  return <Slot/>
-}
+  const isAuthenticated = false;
+  if (!isAuthenticated) return <Redirect href="/sign_in" />;
+  return <Slot />;
+};
 
-export default _layout
+export default _layout;
